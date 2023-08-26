5383485b581d5e86159ec9af0ccddf5adcab3673
Ignacio Hagopian <jsign.uy@gmail.com>
2023-08-26 20:28:19 -0300

    build: add main build
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- build.zig
+++ build.zig
@@ -18,6 +18,29 @@ pub fn build(b: *std.Build) void {
 
     _ = b.addModule("poseidon", Builder.CreateModuleOptions{ .source_file = .{ .path = "src/poseidon.zig" } });
 
+    const exe = b.addExecutable(.{
+        .name = "zig-poseidon",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
     const lib = b.addStaticLibrary(.{
         .name = "zig-poseidon",
         // In this case the main source file is merely a path, however, in more

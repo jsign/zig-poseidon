14cfd661d488e17e00554f705b709b6eb5d5b2fe
Ignacio Hagopian <jsign.uy@gmail.com>
2023-08-26 14:46:57 -0300

    update main with test
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,2 +1,9 @@
 const std = @import("std");
 const testing = std.testing;
+
+test "tests" {
+    std.testing.log_level = .debug;
+
+    _ = @import("poseidon.zig");
+    _ = @import("bn254/tests.zig");
+}
